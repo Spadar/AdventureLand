f7ebda91fc49d81162e9af4fed631d15abf552ef
Spadar <j.paigeNH@gmail.com>
2017-02-22 23:35:34 -0500

    Adding Basic Farming Scripts

---
+++ Class Intro Scripts/Priest/Priest.js
@@ -0,0 +1,161 @@
+game_log("---Script Start---");
+//Put monsters you want to kill in here
+var monsterTargets = ["goo"];
+
+//Movement And Attacking
+setInterval(function () {
+    var lowest_health = lowest_health_partymember();
+    //If we have a target to heal, heal them. Otherwise attack a target.
+    if (lowest_health.health < 0.75) {
+        if (distance_to_point(lowest_health.real_x, lowest_health.real_y) < character.range) {
+            heal(lowest_health);
+        }
+        else {
+            move_to_target(lowest_health);
+        }
+    }
+    else {
+
+        var target = find_viable_targets()[0];
+
+        if (target != null) {
+            if (distance_to_point(target.real_x, target.real_y) < character.range) {
+                if (can_attack(target)) {
+                    attack(target);
+                }
+            }
+            else {
+                move_to_target(target);
+            }
+        }
+    }
+
+}, 100);
+
+//Potions And Looting
+setInterval(function () {
+    loot();
+
+    //Heal With Potions if we're below 75% hp.
+    if (character.hp / character.max_hp < 0.75 || character.mp / character.max_mp < 0.75) {
+        use_hp_or_mp();
+    }
+}, 500);
+
+//Returns the distance of the character to a point in the world.
+function distance_to_point(x, y) {
+    return Math.sqrt(Math.pow(character.real_x - x, 2) + Math.pow(character.real_y - y, 2));
+}
+
+//This function will ether move straight towards the target entity,
+//or utilize smart_move to find their way there.
+function move_to_target(target) {
+    if (can_move_to(target.real_x, target.real_y)) {
+        smart.moving = false;
+        smart.searching = false;
+        move(
+            character.real_x + (target.real_x - character.real_x) / 2,
+            character.real_y + (target.real_y - character.real_y) / 2
+        );
+    }
+    else {
+        if (!smart.searching && !smart.moving) {
+            smart_move({ x: target.real_x, y: target.real_y }, function () { pathing = false; });
+        }
+    }
+}
+//Returns an ordered array of all relevant targets as determined by the following:
+////1. The monsters' type is contained in the 'monsterTargets' array.
+////2. The monster is attacking you or a party member.
+////3. The monster is not targeting someone outside your party.
+//The order of the list is as follows:
+////Monsters attacking you or party members are ordered first.
+////Monsters are then ordered by distance.
+function find_viable_targets() {
+    var monsters = Object.values(parent.entities).filter(
+        mob => (mob.target == null
+                    || parent.party_list.includes(mob.target)
+                    || mob.target == character.name)
+                && (mob.type == "monster"
+                    && (parent.party_list.includes(mob.target)
+                        || mob.target == character.name))
+                    || monsterTargets.includes(mob.mtype));
+
+    for (id in monsters) {
+        var monster = monsters[id];
+
+        if (parent.party_list.includes(monster.target)
+                    || monster.target == character.name) {
+            monster.targeting_party = 1;
+        }
+        else {
+            monster.targeting_party = 0;
+        }
+    }
+
+    //Order monsters by whether they're attacking us, then by distance.
+    monsters.sort(function (current, next) {
+        if (current.targeting_party > next.targeting_party) {
+            return -1;
+        }
+        var dist_current = distance(character, current);
+        var dist_next = distance(character, next);
+        // Else go to the 2nd item
+        if (dist_current < dist_next) {
+            return -1;
+        }
+        else if (dist_current > dist_next) {
+            return 1
+        }
+        else {
+            return 0;
+        }
+    });
+    return monsters;
+}
+
+//Returns the party member with the lowest hp -> max_hp ratio.
+function lowest_health_partymember() {
+    var party = [];
+    if (parent.party_list.length > 0) {
+        var party = parent.party_list.map(
+            function (member) {
+                return {
+                    name: member,
+                    entity: parent.entities[member]
+                }
+            });
+    }
+    else {
+        //Player is not in a party
+        party.push(
+        {
+            name: character.name,
+            entity: character
+        });
+    }
+
+    //Populate health percentages
+    for (id in party) {
+        var member = party[id];
+
+        if (member.name == character.name) {
+            member.entity = character;
+        }
+
+        if (member.entity != null) {
+            member.health = member.entity.hp / member.entity.max_hp;
+        }
+        else {
+            member.health = 1;
+        }
+    }
+
+    //Order our party array by health percentage
+    party.sort(function (current, next) {
+        return current.health - member.health;
+    });
+
+    //Return the lowest health
+    return party[0].entity;
+}
