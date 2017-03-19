6e81baa5269fd93298c2878a913faf1f69946dca
Spadar <j.paigeNH@gmail.com>
2017-03-19 14:24:57 -0400

    Adding state management & potion resupplying

--- Class Intro Scripts/Priest/Priest.js
+++ Class Intro Scripts/Priest/Priest.js
@@ -1,9 +1,70 @@
 game_log("---Script Start---");
 //Put monsters you want to kill in here
-var monster_targets = ["goo"];
+var monster_targets = ["crab"];
+
+var state = "farm";
+
+var min_potions = 50; //The number of potions at which to do a resupply run.
+var purchase_amount = 50;//How many potions to buy at once.
+var potion_types = ["hpot0", "mpot0"];//The types of potions to keep supplied.
 
 //Movement And Attacking
 setInterval(function () {
+	
+	//Determine what state we should be in.
+	state_controller();
+	
+	//Switch statement decides what we should do based on the value of 'state'
+	switch(state)
+	{
+		case "farm":
+			farm();
+			break;
+		case "resupply_potions":
+			resupply_potions();
+			break;
+	}
+}, 100);//Execute 10 times per second
+
+//Potions And Looting
+setInterval(function () {
+    loot();
+
+    //Heal With Potions if we're below 75% hp.
+    if (character.hp / character.max_hp < 0.75 || character.mp / character.max_mp < 0.75) {
+        use_hp_or_mp();
+    }
+}, 500 );//Execute 2 times per second
+
+function state_controller()
+{
+	//Default to farming
+	var new_state = "farm";
+	
+	//Do we need potions?
+	for(type_id in potion_types)
+	{
+		var type = potion_types[type_id];
+		
+		var num_potions = num_items(type);
+		
+		if(num_potions < min_potions)
+		{
+			new_state = "resupply_potions";
+			break;
+		}
+	}
+	
+	if(state != new_state)
+	{
+		state = new_state;
+	}
+	
+}
+
+//This function contains our logic for when we're farming mobs
+function farm()
+{
     var lowest_health = lowest_health_partymember();
     //If we have a target to heal, heal them. Otherwise attack a target.
     if (lowest_health.health < 0.75) {
@@ -28,19 +89,107 @@ setInterval(function () {
                 move_to_target(target);
             }
         }
+		else
+		{
+			if (!smart.moving) {
+				game_log("finding a target");
+            	smart_move({ to: monster_targets[0] });
+        	}
+		}
     }
+}
 
-}, 100);
-
-//Potions And Looting
-setInterval(function () {
-    loot();
-
-    //Heal With Potions if we're below 75% hp.
-    if (character.hp / character.max_hp < 0.75 || character.mp / character.max_mp < 0.75) {
-        use_hp_or_mp();
+//This function contains our logic during resupply runs
+function resupply_potions()
+{
+	var potion_merchant = get_npc("fancypots");
+	
+	var distance_to_merchant = null;
+	
+	if(potion_merchant != null) 
+	{
+		distance_to_merchant = distance_to_point(potion_merchant.position[0], potion_merchant.position[1]);
+	}
+	
+	if (!smart.moving 
+		&& (distance_to_merchant == null || distance_to_merchant > 250)) {
+            smart_move({ to:"potions"});
     }
-}, 500);
+	
+	if(distance_to_merchant != null 
+	   && distance_to_merchant < 250)
+	{
+		buy_potions();
+	}
+}
+
+//Buys potions until the amount of each potion_type we defined in the start of the script is above the min_potions value.
+function buy_potions()
+{
+	if(empty_slots() > 0)
+	{
+		for(type_id in potion_types)
+		{
+			var type = potion_types[type_id];
+			
+			var item_def = parent.G.items[type];
+			
+			if(item_def != null)
+			{
+				var cost = item_def.g * purchase_amount;
+
+				if(character.gold >= cost)
+				{
+					var num_potions = num_items(type);
+
+					if(num_potions < min_potions)
+					{
+						buy(type, purchase_amount);
+					}
+				}
+				else
+				{
+					game_log("Not Enough Gold!");
+				}
+			}
+		}
+	}
+	else
+	{
+		game_log("Inventory Full!");
+	}
+}
+
+
+//Returns the number of items in your inventory for a given item name;
+function num_items(name)
+{
+	var item_count = character.items.filter(item => item != null && item.name == name).reduce(function(a,b){ return a + (b["q"] || 1);
+	}, 0);
+	
+	return item_count;
+}
+
+//Returns how many inventory slots have not yet been filled.
+function empty_slots()
+{
+	var empty = character.items.filter(item => item == null).length;
+	
+	return empty;
+}
+
+//Gets an NPC by name from the current map.
+function get_npc(name)
+{
+	var npc = parent.G.maps[character.map].npcs.filter(npc => npc.id == name);
+	
+	if(npc.length > 0)
+	{
+		return npc[0];
+	}
+	
+	return null;
+}
 
 //Returns the distance of the character to a point in the world.
 function distance_to_point(x, y) {
@@ -59,11 +208,12 @@ function move_to_target(target) {
         );
     }
     else {
-        if (!smart.searching && !smart.moving) {
-            smart_move({ x: target.real_x, y: target.real_y }, function () { pathing = false; });
+        if (!smart.moving) {
+            smart_move({ x: target.real_x, y: target.real_y });
         }
     }
 }
+
 //Returns an ordered array of all relevant targets as determined by the following:
 ////1. The monsters' type is contained in the 'monsterTargets' array.
 ////2. The monster is attacking you or a party member.
