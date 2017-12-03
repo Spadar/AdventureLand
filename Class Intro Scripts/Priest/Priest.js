333003bf94e9f34b851e2a4b497527ed26e1b93a
Spadar <j.paigeNH@gmail.com>
2017-12-03 14:08:24 -0500

    Healing Bugfix

--- Class Intro Scripts/Priest/Priest.js
+++ Class Intro Scripts/Priest/Priest.js
@@ -66,8 +66,9 @@ function state_controller()
 function farm()
 {
     var lowest_health = lowest_health_partymember();
+	game_log(lowest_health.healthRatio);
     //If we have a target to heal, heal them. Otherwise attack a target.
-    if (lowest_health != null && lowest_health.health < 0.75) {
+    if (lowest_health != null && lowest_health.healthRatio < 0.8) {
         if (distance_to_point(lowest_health.real_x, lowest_health.real_y) < character.range) {
             heal(lowest_health);
         }
@@ -268,40 +269,52 @@ function find_viable_targets() {
 function lowest_health_partymember() {
     var party = [];
     if (parent.party_list.length > 0) {
-        var party = parent.party_list.map(
-            function (member) {
-                return {
-                    name: member,
-                    entity: parent.entities[member]
-                }
-            });
+		for(id in parent.party_list)
+		{
+			var member = parent.party_list[id];
+			
+			var entity = parent.entities[member];
+			
+			if(member == character.name)
+			{
+				entity = character;
+			}
+			
+			if(entity != null)
+			{
+				party.push({name: member, entity: entity});
+			}
+		}
     }
-	//Add Self to Party Array
-	party.push(
-    {
-        name: character.name,
-        entity: character
-    });
+	else
+	{
+		//Add Self to Party Array
+		party.push(
+		{
+			name: character.name,
+			entity: character
+		});
+	}
+
     //Populate health percentages
     for (id in party) {
         var member = party[id];
-
-        if (member.name == character.name) {
-            member.entity = character;
-        }
-
         if (member.entity != null) {
-            member.health = member.entity.hp / member.entity.max_hp;
+            member.entity.healthRatio = member.entity.hp / member.entity.max_hp;
         }
         else {
-            member.health = 1;
+            member.healthRatio = 1;
         }
     }
-
+	
+	console.log(party);
+	
     //Order our party array by health percentage
     party.sort(function (current, next) {
-        return current.health - member.health;
+        return current.entity.healthRatio - member.entity.healthRatio;
     });
+	
+	console.log(party);
 
     //Return the lowest health
     return party[0].entity;
